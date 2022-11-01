83560956578eec95574386376c891c7e8823beb5
aybüke <aybukeegrbzz@gmail.com>
2022-11-01 23:36:40 +0300

    02/11

--- src/test/java/GetRequest03.java
+++ src/test/java/GetRequest03.java
@@ -1,3 +1,9 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
 public class GetRequest03 {
 
      /* Matchers ile dataları doğrulayınız
@@ -7,4 +13,19 @@ public class GetRequest03 {
             "last_name": "Morris",
             "avatar": "https://reqres.in/img/faces/5-image.jpg"
      */
+
+    @Test
+    public void test03() {
+        String url="https://reqres.in/api/users";
+        Response response = given().when().get(url);
+        response.then().assertThat().
+                statusCode(200).
+                statusLine("HTTP/1.1 200 OK").
+                contentType(ContentType.JSON);
+
+        response.then().body("data[4].email", equalTo("charles.morris@reqres.in"));
+        response.then().body("data[4].first_name", equalTo("Charles"));
+        response.then().body("data[4].last_name", equalTo("Morris"));
+        response.then().body("data[4].avatar", equalTo("https://reqres.in/img/faces/5-image.jpg"));
+    }
 }
